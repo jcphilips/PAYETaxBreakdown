167e4570cea038fbb50fd9b27dcb2e3ddad7ba6f
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2024-02-12 12:27:26 +0530

    feat: allow user to calculate salary multiple times

--- main.py
+++ main.py
@@ -2,27 +2,42 @@ from TaxProfile import TaxProfile
 import math
 import sys
 
-salary = -math.inf
-while salary < 0:
-    try:
-        salary = float(input("Enter salary: "))
-    except ValueError:
-        print("Must enter floating point values only!: ")
-        salary = -math.inf
-    if salary < 0:
-        print("Cannot enter a value less than 0. Try again.")
-epfContributor = ""
-while epfContributor == "":
-    epfContributor = input("Do you contribute to EPF? (Y/N): ").lower()
-    if epfContributor == "y":
-        user = TaxProfile(salary, True)
-    elif epfContributor == "n":
-        user = TaxProfile(salary, False)
-    else:
-        print("Invalid input!")
-        epfContributor = ""
-if len(sys.argv) > 1:
-    if sys.argv[1] == "r":
-        user.reverse(salary)
-print(user)
- 
+while True:
+    user = None
+    salary = -math.inf
+    while salary < 0:
+        try:
+            salary = float(input("Enter salary: "))
+        except ValueError:
+            print("Must enter floating point values only!: ")
+            salary = -math.inf
+        if salary < 0:
+            print("Cannot enter a value less than 0. Try again.")
+    epfContributor = ""
+    while epfContributor == "":
+        epfContributor = input("Do you contribute to EPF? (Y/N): ").lower()
+        if epfContributor == "y":
+            user = TaxProfile(salary, True)
+        elif epfContributor == "n":
+            user = TaxProfile(salary, False)
+        else:
+            print("Invalid input!")
+            epfContributor = ""
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "r":
+            user.reverse(salary)
+    print(user)
+
+    choice = ""
+    while choice == "":
+        choice = input("Do you want to continue? (Y/N): ").lower()
+        if choice == 'n':
+            print("Thank you for using this program!\nExitting.")
+            sys.exit()
+        elif choice == 'y':
+            print()
+            continue
+        else:
+            choice = ""
+            print("Invalid input!")
+
