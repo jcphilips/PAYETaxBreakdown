e8879492b00e9d942f3501319af7f13b29165e23
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2024-02-16 14:52:24 +0530

    create base functionality and design

--- paye-salary-calculator/src/App.tsx
+++ paye-salary-calculator/src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [baseSalary, setBaseSalary] = useState(0)
-  const [salary, setSalary] = useState(0)
-  const [takeHome, setTakeHome] = useState(0)
+  const [baseSalary, setBaseSalary] = useState(0);
+  const [salary, setSalary] = useState(null);
+  const [takeHome, setTakeHome] = useState(null);
+  const [tax, setTax] = useState(null);
 
   const handleChange = event => {
     if (event.target.id == 'baseSalary') {
@@ -12,18 +13,49 @@ function App() {
     } else if (event.target.id === 'takeHome') {
       setTakeHome(event.target.value);
     } else if (event.target.id === 'salary') {
-      setSalary(event.value.value);
+      setSalary(event.target.value);
     }
   };
 
   const calculateEpf = baseSalary => baseSalary * .08;
 
   const calculateTax = salary => {
-    
+    let calculatedTax = 0
+    if (salary < 100000) {
+        calculatedTax = 0
+    }
+    else if (salary < 141667) {
+        calculatedTax = salary * 0.06 - 6000
+    }
+    else if (salary < 183333) {
+
+        calculatedTax = salary * 0.12 - 14500
+    }
+    else if (salary < 225000) {
+
+        calculatedTax = salary * 0.18 - 25500
+    }
+    else if (salary < 266667) {
+
+        calculatedTax = salary * 0.24 - 39000
+    }
+    else if (salary < 308333) {
+
+        calculatedTax = salary * 0.30 - 55000
+    }
+    else {
+
+        calculatedTax = salary * 0.36 - 73500
+    }
+    setTax(calculatedTax)
   };
 
   const calculateTakeHomeSalary = (baseSalary, salary) => {
-    return salary - calculateEpf(baseSalary) - calculateTax(salary);
+    if (salary !== null && baseSalary !== null) {
+      calculateTax(salary);
+      let calculatedTakeHome = salary - calculateEpf(baseSalary) - tax;
+      setTakeHome(calculatedTakeHome)
+    }
   }
 
 
@@ -32,13 +64,21 @@ function App() {
       <h2>
         Enter base salary if you pay EPF.
       </h2>
-      <form>
-        <input id='baseSalary' type='number' value={baseSalary} onChange={ event => handleChange(event)} ></input>
+      <h3>
+        Enter salary to calculate take home salary after deductions.
+        Enter take home salary to calculate salary before deductions.
+      </h3>
+      <form onSubmit={e => e.preventDefault()}>
+        <input id='baseSalary' type='number' min='0' value={baseSalary} onChange={ event => handleChange(event)} ></input>
         <label htmlFor='baseSalary'>Enter base salary: </label>
-        <input id='takeHome' type='number' value={takeHome} onChange={(event) => handleChange(event)}></input>
+        <input id='salary' type='number' min='0' value={salary} onChange={(event) => handleChange(event)}></input>
+        <label htmlFor='salary'>Enter salary before deductions: </label>
+        <input id='takeHome' type='number' min='0' value={takeHome} onChange={(event) => handleChange(event)}></input>
         <label htmlFor='takeHome'>Enter take home salary: </label>
-        <input id='salary' type='number' value={salary} onChange={(event) => handleChange(event)}></input>
-        <label htmlFor='takeHome'>Enter salary before deductions: </label>
+        <button onClick={ () => {
+          calculateTax(salary);
+          calculateEpf(baseSalary);
+        }}>Click me!</button>
       </form>
     </>
   )
